a7dabead53df204a08f70577ca3370052452da86
morgandrgg <benkim388@gmail.com>
2025-07-27 11:13:34 +0300

    streak

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,8 @@ const Dashboard = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Job Application Tracker</h1>
-      {/* Add job cards here */}
+      <h3>hello world</h3>
+
     </div>
   );
 };
